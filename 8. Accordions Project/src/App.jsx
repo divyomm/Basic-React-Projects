4f1cc2e3aa18397faf1587f4cdbf09c537975cbe
Divyom Singh <110224064+divyomm@users.noreply.github.com>
2023-08-08 21:02:43 +0530

    Added Accordions

---
+++ 8. Accordions Project/src/App.jsx
@@ -0,0 +1,21 @@
+import Accordions from "./Accordions";
+import { accordionData } from "./utils/content";
+
+const App = () => {
+  return (
+    <div>
+      <div className="accordion">
+        {accordionData.map(({ title, content }) => {
+          return (
+            <Accordions
+              title={title}
+              content={content}
+              key={Math.random() * 100}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+export default App;
